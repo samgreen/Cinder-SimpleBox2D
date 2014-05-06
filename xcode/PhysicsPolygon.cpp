0f8a5f01fff817a02b6f8b71b9dbe1b9832cb4b4
Sam Green <greensamuelm@gmail.com>
2014-05-06 05:20:31 -0700

    Added stub for Polygon shape.

---
+++ xcode/PhysicsPolygon.cpp
@@ -0,0 +1,9 @@
+//
+//  PhysicsPolygon.cpp
+//  CinderBoxFun
+//
+//  Created by Sam Green on 5/6/14.
+//
+//
+
+#include "PhysicsPolygon.h"
---
+++ xcode/PhysicsPolygon.h
@@ -0,0 +1,29 @@
+//
+//  PhysicsPolygon.h
+//  CinderBoxFun
+//
+//  Created by Sam Green on 5/6/14.
+//
+//
+
+#pragma once
+
+#include <Box2D/Box2D.h>
+#include "cinder/Cinder.h"
+#include "cinder/CinderMath.h"
+#include "cinder/Vector.h"
+#include "PhysicsBody.h"
+
+namespace Physics {
+    
+    class Polygon : public Physics::Body {
+    public:
+        Polygon();
+        Polygon(const ci::Vec2f &pos);
+        ~Polygon();
+        
+    private:
+    };
+    
+}
+
