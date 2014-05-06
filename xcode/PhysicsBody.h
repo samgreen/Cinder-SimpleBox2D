d5aa85a07d7f959164b1fa2fd7b012d2acca01f8
Sam Green <greensamuelm@gmail.com>
2014-05-06 05:19:25 -0700

    Added a ton of functions to base PhysicsBody.

--- xcode/PhysicsBody.h
+++ xcode/PhysicsBody.h
@@ -22,26 +22,57 @@ namespace Physics {
     } BodyType;
     
     class Body {
-        friend class Physics::World;
     public:
         Body();
         Body(const ci::Vec2f &pos);
         Body(const ci::Vec2f &pos, bool isDynamic);
         ~Body();
-        
-        ci::Vec2f       getPosition();
-        float         getRotation();
-        float           getRotationDegrees();
+
         BodyType        getBodyType();
         
-        void            setDensity(float32 density);
-        void            setRestitution(float32 restitution);
-        void            setFriction(float32 friction);
+        ci::Vec2f       getPosition();
+        void            setPosition(const ci::Vec2f &pos);
+        
+        float           getRotation();
+        void            setRotation(float radians);
+        
+        float           getRotationDegrees();
+        void            setRotationDegrees(float degress);
+        
+        /// Can this body rotate on the Z axis?
+        bool            isRotationLocked();
+        /// Set this body to allow rotation on the Z axis
+        void            setRotationLocked(bool isLocked);
+        
+        /// Is this body affected by the world gravity?
+        bool            isAffectedByGravity();
+        /// Set this body to be affected by the world's gravity
+        void            setAffectedByGravity(bool isAffected);
+        
+        /// Is this body fixed in place or moving?
+        bool            isDynamic();
+        /// Set this body as fixed or affected by physics
+        void            setDynamic(bool isDynamic);
+        
+        float           getDensity();
+        void            setDensity(float density);
+        
+        float           getRestitution();
+        void            setRestitution(float restitution);
+        
+        float           getFriction();
+        void            setFriction(float friction);
+        
+        float           getAngularDamping();
+        void            setAngularDamping(float damping);
+        
     protected:
         BodyType        mBodyType;
         
         b2Body          *mBody;
         b2BodyDef       mBodyDef;
         b2FixtureDef    mFixtureDef;
+        
+        friend class Physics::World;
     };
 }
