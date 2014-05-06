60990c701b84347782b8c29a012dd1aeec9d752a
Sam Green <greensamuelm@gmail.com>
2014-05-06 05:50:51 -0700

    Added triangle vertices placeholder in Polygon constructor.

--- xcode/PhysicsPolygon.cpp
+++ xcode/PhysicsPolygon.cpp
@@ -11,7 +11,18 @@
 namespace Physics {
 #pragma mark - Constructors
     Polygon::Polygon() {}
-    Polygon::Polygon(const ci::Vec2f &pos) {};
+    Polygon::Polygon(const ci::Vec2f &pos) : Body(pos) {
+        // This defines a triangle in CCW order.
+        b2Vec2 vertices[3];
+        vertices[0].Set(0.0f, 0.0f);
+        vertices[1].Set(1.0f, 0.0f);
+        vertices[2].Set(0.0f, 1.0f);
+        int32 count = 3;
+        
+        b2PolygonShape *shape = new b2PolygonShape;
+        shape->Set(vertices, count);
+        mFixtureDef.shape = shape;
+    };
     
 #pragma mark - Destructor
     Polygon::~Polygon() {};
